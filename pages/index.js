541e2c0f662ddd9834135278318b4bc982b4b104
adelaide-ng <zivern88@gmail.com>
2023-12-09 14:04:48 +0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HeroSection from "@/pages/components/HeroSection";
+import Project from "./components/Project";
 
 export default function Home(){
 	return(
-    <div>h1</div>
+    <div className="flex min-h-screen flex-col">
+      <HeroSection />
+      <Project />
+    </div>
 	);
 }
