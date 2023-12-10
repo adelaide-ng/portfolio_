614d05448364f6ef4d0f6bca14cebc4153fbb234
adelaide-ng <zivern88@gmail.com>
2023-12-10 21:11:35 +0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Projects from '@/components/Project';
 import EmailSection from '../components/EmailSection';
 import Footer from '../components/Footer';
 import AchievementsSection from '../components/AchievementsSection';
+import Navbar from '@/components/Navbar';
 
 //Server: not found error handler
 const HeroSection = dynamic(
@@ -16,6 +17,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col ">
         <main>
+        <Navbar />
         <HeroSection />
         <AchievementsSection />
         <AboutMe />
